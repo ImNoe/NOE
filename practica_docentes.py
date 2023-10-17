34f24eabf047b4ed6a92ccb6aae32599721ab5eb
unknown <noe.a57@outlook.com>
2023-10-17 16:55:19 -0600

    Se establecieron las variables y se comenzo a establecer los datos que entregara el programa

--- practica_docentes.py
+++ practica_docentes.py
@@ -1,4 +1,21 @@
 import yaml
 import json
 import xml.etree.ElementTree as ET
+
+
+# Definimos una función para crear un objeto docente
+def crear_docente(nombre, numero_empleado, especialidad):
+    return {
+        "nombre": nombre,
+        "numero_empleado": numero_empleado,
+        "especialidad": especialidad
+    }
+
+# Creamos una lista de docentes
+docentes = [
+    crear_docente("Roberto Perez", 10023, "Haking etico"),
+    crear_docente("Maria Azucena", 10024, "Direccion de proyectos 2"),
+    crear_docente("Patricia Guadalupe", 10025, "Inteligencia de negocios"),
+
+]
  
