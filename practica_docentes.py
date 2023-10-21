1deb2f95a6f07b2546c34d1e1334d4d40544d4b5
Nomad424 <id643095@gmail.com>
2023-10-20 22:55:38 -0600

    Se termino de agregar los docento para que impremiera en los diferentes formatos YAML, JSON Y XML

--- practica_docentes.py
+++ practica_docentes.py
@@ -16,6 +16,33 @@ docentes = [
     crear_docente("Roberto Perez", 10023, "Haking etico"),
     crear_docente("Maria Azucena", 10024, "Direccion de proyectos 2"),
     crear_docente("Patricia Guadalupe", 10025, "Inteligencia de negocios"),
+    crear_docente("Gabriel Alejandro", 10026, "Automatizacion de infraestructura digital"),
+    crear_docente("Alan Dias", 10027, "sistemas de calidad"),
+    crear_docente("Geltrudis pilar", 10028, "Ingles"),
+    crear_docente("Roberto Perez", 10029, "Tutorias"),
+    crear_docente("Adriana", 10030, "Equipos de alto rendimiento"),
 
 ]
+# Serializamos los datos en YAML
+yaml_data = yaml.dump(docentes, default_flow_style=False)
+
+# Serializamos los datos en JSON
+json_data = json.dumps(docentes)
+
+# Serializamos los datos en XML
+xml_data = ET.tostring(
+    ET.Element("docentes", attrib={"total": str((docentes))}),
+    encoding="utf-8"
+)
+
+# Imprimimos los datos serializados
+print("YAML:")
+print(yaml_data)
+
+print("JSON:")
+print(json_data)
+print("\n")
+
+print("XML:")
+print(xml_data)
  
